42518e3009ab1b01d52a166bad01ed28f37f3755
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-08-14 11:38:09 +0200

    Configured AqGR-lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountriesTableComponent } from './components/countries/countries-table/countries-table.component';
 import { CountriesComponent } from './pages/countries/countries.component';
+import { AqgrLibModule } from 'aqgr-lib';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,9 +17,13 @@ import { CountriesComponent } from './pages/countries/countries.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    AqgrLibModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+        {provide: 'LoggingEnabled', useValue: environment.logging}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
