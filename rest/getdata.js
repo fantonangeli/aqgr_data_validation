7466d4cfa0560435411318aed9e49be758279244
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-08-27 10:31:46 +0200

    Fixed error deprecated res.send

--- rest/getdata.js
+++ rest/getdata.js
@@ -1,15 +1,17 @@
 'use strict'
 
 module.exports = (req, res, next) => {
-  const _send = res.send
+  const _send = res.send;
   res.send = function (body) {
       try {
         const json = JSON.parse(body)
         if (Array.isArray(json)) {
           if (json.length === 1 && json[0].data) {
-            return _send.call(this, JSON.stringify(json[0].data))
+            // return _send.call(this, JSON.stringify(json[0].data))
+              return res.status(200).send.call(this, JSON.stringify(json[0].data));
           } else if (json.length === 0) {
-            return _send.call(this, '{}', 404)
+            // return _send.call(this, '{}', 404)
+              return res.status(404).send.call(this, {});
           }
         }else{
             _send.call(this, body)
