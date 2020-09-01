f6481930454062cb27391c7a817604234f53b6b9
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-09-01 11:12:20 +0200

    Removed unused sftypes json

--- rest/db.js
+++ rest/db.js
@@ -2,7 +2,6 @@ const countries = require("../src/assets/data/countries/countries.json");
 const countryInfo = require("../src/assets/data/country/country-info.json");
 const species = require("../src/assets/data/country/species.json");
 const ftypes = require("../src/assets/data/specie/ftypes.json");
-const sftypes = require("../src/assets/data/ftype/sftypes.json");
 const specieInfo = require("../src/assets/data/specie/specie-info.json");
 const ftypeInfo = require("../src/assets/data/ftype/ftype-info.json");
 
