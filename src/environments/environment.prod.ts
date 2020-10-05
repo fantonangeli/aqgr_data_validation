b9a5a578b73651ed6fcc4244f6e28a3456055e94
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-10-05 17:13:58 +0200

    Production Environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,53 @@
+const baseUrl="https://aqgr-dv-demo-rest.herokuapp.com/";
+// const dataPath="assets/data/";
+// const countriesPath=dataPath+"countries/";
+// const speciesPath=dataPath+"species/";
+// const ftypesPath=dataPath+"ftypes/";
+
 export const environment = {
-  production: true
+    production: true,
+    logging:true,
+
+    /**
+     * default date format used to format dates
+     */
+    defaultDateFormat:'dd/MM/yyyy hh:mm',
+
+    services:{
+        countries:{
+            all:baseUrl+"countries",
+            info:baseUrl+"country-info",
+        },
+        species:{
+            all:baseUrl+"species",
+            info:baseUrl+"specie-info",
+            edit:(id)=>`${baseUrl}species/${id}`,
+        },
+        ftypes:{
+            all:baseUrl+"ftypes",
+            edit:(id)=>`${baseUrl}ftypes/${id}`,
+            info:baseUrl+"ftype-info",
+        },
+        params: {
+            country:"_country_like",
+            alphaCode:"alphaCode_like",
+            specie:"_specie_like",
+            ftype:"_ftypeParent_like",
+            sftype:"sftype_like",
+            limit:"_limit",
+            sortBy:"_sort"
+        },
+    },
+
+    /**
+     * statuses of the records
+     */
+    statuses:{
+        accepted:"accepted",
+        rejected:"rejected",
+        published:"published",
+        new:"new",
+        pending:"pending"
+    }
+
 };
