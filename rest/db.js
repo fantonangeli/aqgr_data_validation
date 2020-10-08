f15c08490834d1cc822cc843d57161e31ed2cf54
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-10-08 10:51:12 +0200

    Moved rest data to /rest folder

--- rest/db.js
+++ rest/db.js
@@ -1,9 +1,11 @@
-const countries = require("../src/assets/data/countries/countries.json");
-const countryInfo = require("../src/assets/data/countries/country-info.json");
-const species = require("../src/assets/data/species/species.json");
-const specieInfo = require("../src/assets/data/species/specie-info.json");
-const ftypes = require("../src/assets/data/ftypes/ftypes.json");
-const ftypeInfo = require("../src/assets/data/ftypes/ftype-info.json");
+const dataPath="./data/";
+
+const countries = require(dataPath+"countries/countries.json");
+const countryInfo = require(dataPath+"countries/country-info.json");
+const species = require(dataPath+"species/species.json");
+const specieInfo = require(dataPath+"species/specie-info.json");
+const ftypes = require(dataPath+"ftypes/ftypes.json");
+const ftypeInfo = require(dataPath+"ftypes/ftype-info.json");
 
 module.exports = () => ({
     "country-info":countryInfo,
