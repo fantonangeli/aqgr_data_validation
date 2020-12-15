604a1eb8efe7a609503b61c4640b2e984a05fd93
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-12-15 18:36:38 +0700

    Use of BasePage01Component

--- src/app/pages/country/country.component.ts
+++ src/app/pages/country/country.component.ts
@@ -1,45 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { CountryInfoService } from 'src/app/services/country/country-info.service';
 import { environment } from "src/environments/environment";
 import { LoggerService, SearchServiceParams, BaseService } from "aqgr-lib";
+import { BasePage01Component } from 'src/app/components/base-page01.component';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss']
 })
-export class CountryComponent implements OnInit {
-    iso3:string;
-    countryName="";
-    searchServiceParams=new SearchServiceParams();
-    info={};
+export class CountryComponent extends BasePage01Component implements OnInit {
 
-  constructor(private route: ActivatedRoute, private _countryInfoService:CountryInfoService, private _logger:LoggerService) { }
-    
+    constructor(injector: Injector, service: CountryInfoService){
+        super(injector, service);
+    }
+
     /**
-     * fetch the data and load them
-     * @param iso3 country code
-     *
+     * event executed when element info are loaded
      */
-    fetchInfo(iso3:string) {
-        this._countryInfoService.getData(this.iso3).subscribe(
-            (data)=>{
-                this.countryName=data.nameEn;
-                this.info=data;
-                this.searchServiceParams.country=this.countryName;
-            },
-            (error)=>{
-                this._logger.error("Network error: ", error);
-            }
-        );
-
+    onInfoLoaded(){
+        this.searchServiceParams.country=this.info.nameEn;
     }
-
-    ngOnInit(){
-        this.iso3 = this.route.snapshot.paramMap.get("iso3");
-
-        this.fetchInfo(this.iso3);
-    }
-
 }
